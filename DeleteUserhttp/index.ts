a1869f8a30d64162c8fe6c95c69050fa78fbc7c2
Michael Mora <maycolmora99@gmail.com>
2023-02-13 10:54:57 -0500

    Edit delete azure function

--- DeleteUserhttp/index.ts
+++ DeleteUserhttp/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { appDataSource } from "../entities/initDataSource";
 import { Users } from "../entities/Users.entity";
+import { WorkDevices } from "../entities/WorkDevices.entity";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -19,22 +20,56 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     }
 
     await appDataSource.initialize();
-    const user = await appDataSource.manager.findOne(Users, {
-        where: {
-            userId,
-        }
-    })
-    const response = await appDataSource.manager.delete(Users, user);
-    
-    context.res = {
-        status: 200, /* Defaults to 200 */
-        body: {
-            msg: "success",
-            data: {
+
+    try {
+        const user = await appDataSource.manager.findOne(Users, {
+            where: {
                 userId,
+            }
+        });
+        const workDevice = await appDataSource.manager.findOne(WorkDevices, {
+            where: {
+                userId,
+            }
+        });
+        context.log(user);
+        if (!user) {
+                context.res = {
+                    status: 400,
+                    body: {
+                        msg: "bad_request",
+                        data: {},
+                    },
+                };
+                await appDataSource.destroy();
+
+                return;
+        }
+        
+        if (workDevice) {
+            await appDataSource.manager.delete(WorkDevices, workDevice);
+        }
+        await appDataSource.manager.delete(Users, user);
+        
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: {
+                msg: "success",
+                data: {
+                    userId,
+                },
             },
-        },
-    };
+        };
+    } catch (error) {
+        context.log(error);
+        context.res = {
+            status: 500, /* Defaults to 200 */
+            body: {
+                msg: "error",
+                data: JSON.stringify(error),
+            },
+        };
+    }
     await appDataSource.destroy();
 };
 
